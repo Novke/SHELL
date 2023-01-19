b3c982633cd7257172d0ef002fe440e5dea77712
Novke <novke.brate@gmail.com>
2023-01-19 12:38:53 +0100

    1. Commit

---
+++ Server/src/logika/Kontroler.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logika;
+
+import baza.DBBroker;
+
+public class Kontroler {
+
+    private static Kontroler instance;
+    private DBBroker dbb;
+
+    private Kontroler() {
+        dbb = new DBBroker();
+    }
+
+    public static Kontroler getInstance() {
+        if (instance == null) {
+            instance = new Kontroler();
+        }
+        return instance;
+    }
+
+}
